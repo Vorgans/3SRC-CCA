665c19bf2b36fa9f6654e828a1448b7a7136b7a3
CHRCAP <ccappeliez@myges.fr>
2022-12-06 12:21:51 +0100

    update script V2

--- script.sh
+++ script.sh
@@ -10,5 +10,35 @@ cp ./config__files/ssh/Banner /etc/Banner
 
 # Redemarrer le service sshd 
 
-systemctl restart sshd 
+systemctl restart sshd
 
+#Création d'utilisateur
+
+printf "Donnez le nom de l'utilisateur à créer :\n"
+
+read user
+
+useradd -m -d/home/$user -s /bin/bash -c "$user" $user && passwd $user && usermod -aG sudo $user && echo -e $user "est devenu un superUser !\n"
+
+
+#Insertion de la clé public
+
+printf "Entrez la clé public de l'utilisateur : \n"
+
+read key
+
+echo $key >> tmp_authorized_keys.txt
+
+cat tmp_authorized_keys.txt >> ~/.ssh/authorized_keys && echo "Clé ssh ajouté \n"
+rm tmp_authorized_keys.txt && echo "Fichier temporaire de clé supprimé \n"
+service sshd restart && echo "Opération effectuée.... \n"
+
+printf "Voulez-vous vous deconnecter ? (y/yes/n/no)"
+
+read answer
+
+if [[ "$answer" = yes || "$answer" = y  ]]; then
+    exit
+else
+    exit 0
+fi
