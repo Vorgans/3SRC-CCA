e7e311e48a31d4b4970751f6d285275e9e26ca82
CHRCAP <ccappeliez@myges.fr>
2022-12-06 22:44:12 +0100

    add mkdir for authorize key

--- script.sh
+++ script.sh
@@ -20,6 +20,8 @@ read user
 
 useradd -m -d/home/$user -s /bin/bash -c "$user" $user && passwd $user && usermod -aG sudo $user && echo -e $user "est devenu un superUser !\n"
 
+#Création du dossier pour l'authorisation SSH
+mkdir /home/$user/.ssh/ && echo -e "\e[1;33mCréation du dossier SSH\e[0m \n"
 
 #Insertion de la clé public
 
@@ -27,9 +29,6 @@ printf "Entrez la clé public de l'utilisateur : \n"
 
 read key
 
-echo $key >> tmp_authorized_keys.txt
-
-cat tmp_authorized_keys.txt >> ~/.ssh/authorized_keys && echo -e "\e[1;33mClé ssh ajouté\e[0m \n"
-rm tmp_authorized_keys.txt && echo -e "\e[1;31mFichier temporaire de clé supprimé\e[0m \n"
+echo $key >> /home/$user/.ssh/authorized_keys && echo -e "\e[1;33mClé ssh ajouté\e[0m \n"
 sleep 1
 service sshd restart && echo -e "\e[1;32mOpération effectuée....\e[0m \n"
